256593f64c1f8b830a5160d431ee5c27e07432a2
Alireza Farhanian <aariow01@gmail.com>
2024-01-12 21:02:27 +0330

    Add loading env to main.go

--- main.go
+++ main.go
@@ -1,11 +1,27 @@
 package main
 
 import (
+	"fmt"
 	"os"
+
+	"github.com/joho/godotenv"
 
 	"todo_list/cmd"
 )
 
+var projectDir, _ = os.Getwd()
+
 func main() {
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Println("Recovered. Error:\n", r)
+		}
+	}()
+
+	err := godotenv.Load(projectDir + "/.env")
+	if err != nil {
+		panic("Env variables could not be injected.")
+	}
+
 	cmd.Run(os.Args)
 }
